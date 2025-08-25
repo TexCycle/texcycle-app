b45096844d73c96aed38cea3c6267917473cccff
Eduardo Farias Camargo <edu.farias.camargo@gmail.com>
2025-08-25 18:56:20 -0300

    feat(frontend): finalize initial map screen

--- frontend/lib/core/routing/app_router.dart
+++ frontend/lib/core/routing/app_router.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/features/auth/presentation/pages/signin_page.dart';
+import 'package:frontend/features/map/presentation/pages/map_page.dart';
 import 'package:go_router/go_router.dart';
 import '../../features/auth/presentation/pages/signup_page.dart';
 
 GoRouter buildRouter() => GoRouter(
-  initialLocation: '/signup',
+  initialLocation: '/map',
   routes: [
     GoRoute(path: '/signup', builder: (_, __) => const SignUpPage()),
     GoRoute(path: '/signin', builder: (_, __) => const SignInPage()),
+    GoRoute(path: '/map', builder: (_, __) => const MapPage()),
   ],
   errorBuilder: (_, state) => Scaffold(
     appBar: AppBar(title: const Text('Ops')),
