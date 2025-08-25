8f92985ea68fd035de2c40f305cba590fae780ef
Eduardo Farias Camargo <edu.farias.camargo@gmail.com>
2025-08-25 19:32:37 -0300

    Revert "Merge pull request #7 from TexCycle/feature/chat-screen"
    This reverts commit bb5e04665fe6e1c7ed026568be3ec0b3c3382b87, reversing
    changes made to 36556bdc85b2fe7be1ce06ddb91766510930f08d.

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -1,18 +1,30 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:frontend/app.dart';
+import 'package:frontend/main.dart';
 
 void main() {
-  testWidgets('App loads and shows Login screen', (WidgetTester tester) async {
-    // Build our app
-    await tester.pumpWidget(const TextileApp());
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
 
-    // Verify that LoginPage loads with title "Entrar"
-    expect(find.text('Entrar'), findsOneWidget);
-    expect(find.text('Bem-vindo(a)'), findsOneWidget);
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
 
-    // Check if the "Criar conta" button exists
-    expect(find.text('Criar conta'), findsOneWidget);
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
