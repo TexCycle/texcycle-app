b33253cbd2478799518f458314d309ce5794403c
Eduardo Farias Camargo <edu.farias.camargo@gmail.com>
2025-08-27 20:15:57 -0300

    feat(frontend): finalize initial my donor info screen

--- frontend/lib/core/routing/app_router.dart
+++ frontend/lib/core/routing/app_router.dart
@@ -2,18 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:frontend/features/account/presentation/pages/account_page.dart';
 import 'package:frontend/features/auth/presentation/pages/signin_page.dart';
 import 'package:frontend/features/chat/presentation/pages/chat_page.dart';
+import 'package:frontend/features/donor/presentation/pages/donor_info_page.dart';
 import 'package:frontend/features/map/presentation/pages/map_page.dart';
 import 'package:go_router/go_router.dart';
 import '../../features/auth/presentation/pages/signup_page.dart';
 
 GoRouter buildRouter() => GoRouter(
-  initialLocation: '/signup',
+  initialLocation: '/donorinfo',
   routes: [
     GoRoute(path: '/signup', builder: (_, __) => const SignUpPage()),
     GoRoute(path: '/signin', builder: (_, __) => const SignInPage()),
     GoRoute(path: '/map', builder: (_, __) => const MapPage()),
     GoRoute(path: '/chat', builder: (_, __) => const ChatPage()),
     GoRoute(path: '/myaccount', builder: (_, __) => const AccountPage()),
+    GoRoute(path: '/donorinfo', builder: (_, __) => const DonorInfoPage()),
   ],
   errorBuilder: (_, state) => Scaffold(
     appBar: AppBar(title: const Text('Ops')),
